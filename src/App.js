78ccbc6fd243e128b34555e84c290317e89b1e77
AliRadmanesh <ali.radmanesh1995@gmail.com>
2021-09-28 00:41:30 +0330

    Test react-router usage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.scss';
 
 function App() {
+  return (
+    <Router>
+      <div>
+        <ul className='flex flex-row'>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+function Home() {
   return (
     <div className="text-center">
       <header className="App-header flex flex-col justify-center items-center">
@@ -55,4 +93,10 @@ function App() {
   );
 }
 
-export default App;
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
