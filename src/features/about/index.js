e0c5c2a80bc25c515c8adeac4772b212b39be0d3
Milad Khorramian <mahdiar.khorramian@yahoo.com>
2021-10-09 01:34:57 +0330

    Grid Fixation on About page

--- src/features/about/index.js
+++ src/features/about/index.js
@@ -15,72 +15,79 @@ export default function About() {
       <PageHeader title="سئوالات متداول" text="شاید سئوال شما هم بین سئوالات متداول کارساز باشد" />
       <div className="container">
         <div id="about" className={styles.aboutContainer}>
-          <div className={styles.aboutMain}>
-            <h3 className={styles.aboutTitle}>رسالت کارساز</h3>
-            <p className={styles.aboutDescription}>
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان
-              گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای
-              شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می
-              باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان
-              را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان
-              خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و
-              دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل
-              حروفچینی دستاوردهای اصلی، و .جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد
-              استفاده قرار گیرد
-            </p>
+          <div>
+            <div className={styles.aboutMain}>
+              <h3 className={styles.aboutTitle}>رسالت کارساز</h3>
+              <p className={styles.aboutDescription}>
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان
+                گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و
+                برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی
+                می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و
+                متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
+                الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید
+                داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان
+                مورد نیاز شامل حروفچینی دستاوردهای اصلی، و .جوابگوی سوالات پیوسته اهل دنیای موجود
+                طراحی اساسا مورد استفاده قرار گیرد
+              </p>
+            </div>
+
+            <div className={styles.aboutImage}>
+              <img src={image} alt="" className="tw-w-full tw-mb-4 tw-block lg:tw-hidden" />
+            </div>
+
+            <div className={styles.aboutList}>
+              <h3 className={styles.aboutTitle}>ویژگی‌های کارساز</h3>
+              <ul className={styles.aboutDescription}>
+                <li className="tw-flex tw-items-center tw-mb-2">
+                  <img
+                    style={{ transform: 'rotate(90deg)', width: '18px' }}
+                    src={arrow}
+                    alt=""
+                    className="tw-ml-4"
+                  />
+                  <span>تنوع گروه های آموزشی در کازساز</span>
+                </li>
+                <li className="tw-flex tw-items-center tw-mb-2">
+                  <img
+                    style={{ transform: 'rotate(90deg)', width: '18px' }}
+                    src={arrow}
+                    alt=""
+                    className="tw-ml-4"
+                  />
+                  <span>آموزش های آموزشگاه های برتر در کارساز</span>
+                </li>
+                <li className="tw-flex tw-items-center tw-mb-2">
+                  <img
+                    style={{ transform: 'rotate(90deg)', width: '18px' }}
+                    src={arrow}
+                    alt=""
+                    className="tw-ml-4"
+                  />
+                  <span>مدرسان برتر در زمینه های مختلف</span>
+                </li>
+                <li className="tw-flex tw-items-center tw-mb-2">
+                  <img
+                    style={{ transform: 'rotate(90deg)', width: '18px' }}
+                    src={arrow}
+                    alt=""
+                    className="tw-ml-4"
+                  />
+                  <span>شارژ شدن کیف پول و برداشت از کیف پول برای خرید های شما</span>
+                </li>
+                <li className="tw-flex tw-items-center tw-mb-2">
+                  <img
+                    style={{ transform: 'rotate(90deg)', width: '18px' }}
+                    src={arrow}
+                    alt=""
+                    className="tw-ml-4"
+                  />
+                  <span>ارائه بهترین فرصت های شغلی برای کاربران</span>
+                </li>
+              </ul>
+            </div>
           </div>
           <div className={styles.aboutImage}>
-            <img src={image} alt="" />
-          </div>
-          <div className={styles.aboutList}>
-            <h3 className={styles.aboutTitle}>ویژگی‌های کارساز</h3>
-            <ul className={styles.aboutDescription}>
-              <li className="tw-flex tw-items-center tw-mb-2">
-                <img
-                  style={{ transform: 'rotate(90deg)', width: '18px' }}
-                  src={arrow}
-                  alt=""
-                  className="tw-ml-4"
-                />
-                <span>تنوع گروه های آموزشی در کازساز</span>
-              </li>
-              <li className="tw-flex tw-items-center tw-mb-2">
-                <img
-                  style={{ transform: 'rotate(90deg)', width: '18px' }}
-                  src={arrow}
-                  alt=""
-                  className="tw-ml-4"
-                />
-                <span>آموزش های آموزشگاه های برتر در کارساز</span>
-              </li>
-              <li className="tw-flex tw-items-center tw-mb-2">
-                <img
-                  style={{ transform: 'rotate(90deg)', width: '18px' }}
-                  src={arrow}
-                  alt=""
-                  className="tw-ml-4"
-                />
-                <span>مدرسان برتر در زمینه های مختلف</span>
-              </li>
-              <li className="tw-flex tw-items-center tw-mb-2">
-                <img
-                  style={{ transform: 'rotate(90deg)', width: '18px' }}
-                  src={arrow}
-                  alt=""
-                  className="tw-ml-4"
-                />
-                <span>شارژ شدن کیف پول و برداشت از کیف پول برای خرید های شما</span>
-              </li>
-              <li className="tw-flex tw-items-center tw-mb-2">
-                <img
-                  style={{ transform: 'rotate(90deg)', width: '18px' }}
-                  src={arrow}
-                  alt=""
-                  className="tw-ml-4"
-                />
-                <span>ارائه بهترین فرصت های شغلی برای کاربران</span>
-              </li>
-            </ul>
+            <img src={image} alt="" className="tw-w-full tw-mb-4 tw-hidden lg:tw-block" />
           </div>
         </div>
       </div>
