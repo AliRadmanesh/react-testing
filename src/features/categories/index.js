392d9928a519d50472ced5237b3eee56f687f955
Milad Khorramian <mahdiar.khorramian@yahoo.com>
2021-10-09 15:41:42 +0330

    Unneccessary lines removed.

--- src/features/categories/index.js
+++ src/features/categories/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import toast from 'react-hot-toast';
 import axios from 'axios';
 import PageHeader from '../../components/global/PageHeader';
 import CategoryCard from '../../components/global/CategoryCard';
