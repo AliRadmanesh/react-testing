27b22ebb38364655efcfcd53779e57c228e50024
Milad Khorramian <mahdiar.khorramian@yahoo.com>
2021-10-25 00:29:36 +0330

    Checkbox.js under src/common/template updated

--- src/common/template/Checkbox.js
+++ src/common/template/Checkbox.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './template.css';
 import indicator from './checkbox-indicator.svg';
 
-export default function Checkbox({ onChange, text = '', disabled, classes = '' }) {
+export default function Checkbox({ onChange, text = '', disabled, classes = '', checked }) {
   return (
     <div className={`template ${classes}`}>
       {/* eslint-disable-next-line */}
       <label class="checkbox-container">
         <span>{text}</span>
-        <input type="checkbox" className="checkbox" onChange={onChange} disabled={disabled} />
+        <input
+          type="checkbox"
+          className="checkbox"
+          onChange={onChange}
+          disabled={disabled}
+          checked={checked}
+        />
         <span className="checkmark" />
         <img src={indicator} alt="" className="checkmark-indicator" />
       </label>
