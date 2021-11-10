849793aa73d2b3fefcf054ace44833b6959d9e04
Milad Khorramian <mahdiar.khorramian@yahoo.com>
2021-11-10 10:19:47 +0330

    Search aaded to Jobs index page

--- src/features/jobs/index.js
+++ src/features/jobs/index.js
@@ -3,8 +3,13 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import Layout from '../../components/jobs/Layout';
 import Home from '../../components/jobs/Home';
+import Search from '../../components/jobs/Search';
 
-import { getJobsData } from '../../app/redux/actions/jobsActions';
+import {
+  getJobsData,
+  getJobsFilterOptions,
+  setJobsSection,
+} from '../../app/redux/actions/jobsActions';
 
 import './jobs.css';
 
@@ -13,19 +18,19 @@ export default function JobListPage() {
   const { section } = useSelector((state) => state.jobs);
 
   useEffect(() => {
+    dispatch(getJobsFilterOptions());
     if (new URL(window.location).search === '') {
+      dispatch(setJobsSection(1));
       dispatch(getJobsData());
+    } else {
+      dispatch(setJobsSection(2));
     }
   }, [new URL(window.location).search]);
 
   return (
     <Layout>
       {section === 1 && <Home />}
-      {section === 2 && (
-        <div>
-          <h1>Job search</h1>
-        </div>
-      )}
+      {section === 2 && <Search />}
     </Layout>
   );
 }
