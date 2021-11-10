89f33c7bb56926d88d04cbb48fcde177be0048fb
Milad Khorramian <mahdiar.khorramian@yahoo.com>
2021-11-10 10:12:51 +0330

    Jobs redux updated to implement adding and removing filters on search

--- src/app/redux/actions/jobsActions.js
+++ src/app/redux/actions/jobsActions.js
@@ -26,3 +26,84 @@ export const getJobsData = () => async (dispatch) => {
     );
   }
 };
+
+// export const searchJobs = () => async (dispatch) => {
+//   try {
+//     const res = await instance.get('/api/v1/web/service/jobs/search/?q=ui');
+
+//     if(res.data.)
+//   } catch (error) {}
+// };
+
+export const getJobsFilterOptions = () => async (dispatch) => {
+  try {
+    const res = await instance.get('/api/v1/web/content/jobs/search-content');
+    if (res.status === 200 || res.status === 201) {
+      dispatch({
+        type: 'GET_JOBS_FILTER_OPTIONS',
+        payload: res.data.data,
+      });
+    } else {
+      toast.error('خطای دریافت اطلاعات');
+    }
+  } catch (error) {
+    toast.error(
+      'خطا پیش از فرایند دریافت اطلاعات. اتصال خود به شبکه را بررسی نموده و دوباره تلاش نمایید',
+    );
+  }
+};
+
+export const addJobsSalaryFilter = (object) => (dispatch) => {
+  dispatch({
+    type: 'ADD_JOBS_SEARCH_SALARY_FILTER',
+    payload: { id: object.id, title: object.title },
+  });
+};
+
+export const removeJobsSalaryFilter = (id) => (dispatch) => {
+  dispatch({
+    type: 'REMOVE_JOBS_SEARCH_SALARY_FILTER',
+    payload: id,
+  });
+};
+
+export const addJobsContractFilter = (object) => (dispatch) => {
+  dispatch({
+    type: 'ADD_JOBS_SEARCH_Contract_FILTER',
+    payload: { id: object.id, title: object.title },
+  });
+};
+
+export const removeJobsContractFilter = (id) => (dispatch) => {
+  dispatch({
+    type: 'REMOVE_JOBS_SEARCH_Contract_FILTER',
+    payload: id,
+  });
+};
+
+export const addJobsExperianceFilter = (object) => (dispatch) => {
+  dispatch({
+    type: 'ADD_JOBS_SEARCH_EXPERIANCE_FILTER',
+    payload: { id: object.id, title: object.title },
+  });
+};
+
+export const removeJobsExperianceFilter = (id) => (dispatch) => {
+  dispatch({
+    type: 'REMOVE_JOBS_SEARCH_EXPERIANCE_FILTER',
+    payload: id,
+  });
+};
+
+export const clearJobsSearchFilters = () => (dispatch) => {
+  dispatch({
+    type: 'CLEAR_JOBS_FILTERS',
+  });
+};
+
+export const showJobsMobileMenu = (bool) => (dispatch) => {
+  dispatch({
+    type: 'SHOW_JOBS_FILTER_MOBILE',
+    payload: bool,
+  });
+};
--- src/app/redux/reducers/jobsReducer.js
+++ src/app/redux/reducers/jobsReducer.js
@@ -2,7 +2,20 @@ const initial = {
   section: 1,
   recommended: [],
   recent: [],
-  search: {},
+  search: {
+    options: {
+      contract_types: [],
+      work_experiences: [],
+      salary_ranges: [],
+    },
+    filters: {
+      contract_types: [],
+      work_experiences: [],
+      salary_ranges: [],
+    },
+    result: [],
+    mobile: false,
+  },
 };
 
 export default (state = initial, action) => {
@@ -18,6 +31,109 @@ export default (state = initial, action) => {
       return {
         ...state,
         section: action.payload,
+      };
+    case 'GET_JOBS_FILTER_OPTIONS':
+      return {
+        ...state,
+        search: {
+          ...state.search,
+          options: action.payload,
+        },
+      };
+    case 'ADD_JOBS_SEARCH_SALARY_FILTER':
+      return {
+        ...state,
+        search: {
+          ...state.search,
+          filters: {
+            ...state.search.filters,
+            salary_ranges: [...state.search.filters.salary_ranges, action.payload],
+          },
+        },
+      };
+    case 'ADD_JOBS_SEARCH_CONTRACT_FILTER':
+      return {
+        ...state,
+        search: {
+          ...state.search,
+          filters: {
+            ...state.search.filters,
+            contract_types: [...state.search.filters.contract_types, action.payload],
+          },
+        },
+      };
+    case 'ADD_JOBS_SEARCH_EXPERIANCE_FILTER':
+      return {
+        ...state,
+        search: {
+          ...state.search,
+          filters: {
+            ...state.search.filters,
+            work_experiences: [...state.search.filters.work_experiences, action.payload],
+          },
+        },
+      };
+    case 'REMOVE_JOBS_SEARCH_SALARY_FILTER':
+      return {
+        ...state,
+        search: {
+          ...state.search,
+          filters: {
+            ...state.search.filters,
+            salary_ranges: state.search.filters.salary_ranges.filter(
+              (salary) => salary.id !== action.payload,
+            ),
+          },
+        },
+      };
+    case 'REMOVE_JOBS_SEARCH_EXPERIANCE_FILTER':
+      return {
+        ...state,
+        search: {
+          ...state.search,
+          filters: {
+            ...state.search.filters,
+            work_experiences: state.search.filters.work_experiences.filter(
+              (experiences) => experiences.id !== action.payload,
+            ),
+          },
+        },
+      };
+    case 'REMOVE_JOBS_SEARCH_CONTRACT_FILTER':
+      return {
+        ...state,
+        search: {
+          ...state.search,
+          filters: {
+            ...state.search.filters,
+            contract_types: state.search.filters.contract_types.filter(
+              (type) => type.id !== action.payload,
+            ),
+          },
+        },
+      };
+    case 'CLEAR_COURSES_FILTERS':
+      return {
+        ...state,
+        filters: { course_types: [], academies: [] },
+      };
+
+    case 'SHOW_JOBS_FILTER_MOBILE':
+      return {
+        ...state,
+        search: {
+          ...state.search,
+          mobile: action.payload,
+        },
+      };
+
+    case 'CLEAR_JOBS_FILTERS':
+      return {
+        ...state,
+        search: {
+          ...state.search,
+          filters: initial.search.filters,
+        },
       };
     default:
       return state;
