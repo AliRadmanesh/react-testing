1560c70d9bcc6c358a9d0e184bc9fbbf110a5ad6
Milad Khorramian <mahdiar.khorramian@yahoo.com>
2021-11-13 22:50:48 +0330

    Scroll to top btn updated to contain 'scroll-to-top-button' as a class.

--- src/components/global/ScrollToTop.js
+++ src/components/global/ScrollToTop.js
@@ -12,12 +12,12 @@ export default function ScrollToTop() {
   }, []);
   return (
     <button
-      className="tw-fixed tw-bottom-4 tw-p-4 bg-primary"
+      className="tw-fixed tw-bottom-4 tw-p-4 bg-primary scroll-to-top-button"
       onClick={() => window.scrollTo(0, 0)}
       style={{
         left: scroll !== (0, 0) ? '1rem' : '-10rem',
         transition: 'left .85s ease-out',
-        zIndex: '9999',
+        zIndex: '999999',
       }}
     >
       <img src={arrow} alt="" style={{ transform: 'rotate(90deg)' }} />
