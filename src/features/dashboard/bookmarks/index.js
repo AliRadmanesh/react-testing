f993327ac7853b4b1746f90bbba1041d7555888a
Milad Khorramian <mahdiar.khorramian@yahoo.com>
2021-12-04 02:28:53 +0330

    Dashboard pages created

---
+++ src/features/dashboard/bookmarks/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Bookmarks() {
+  return (
+    <div>
+      <h1>Dashboard Bookmarks Page</h1>
+    </div>
+  );
+}
---
+++ src/features/dashboard/wallet/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Wallet() {
+  return (
+    <div>
+      <h1>Dashboard Wallet Page</h1>
+    </div>
+  );
+}
