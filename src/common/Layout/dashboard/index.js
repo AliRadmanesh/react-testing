41a46660ef4ff42d71290fa7a6a2a5ef25cbbf40
Milad Khorramian <mahdiar.khorramian@yahoo.com>
2021-12-05 11:47:14 +0330

    dashboard layout's gap between desktop navigation and content increased

--- src/common/Layout/dashboard/index.js
+++ src/common/Layout/dashboard/index.js
@@ -52,7 +52,7 @@ export default function Layout({ children }) {
         <NavigationMobile />
       </div>
       <div className="bg-light tw-shadow-inner tw-pt-8" style={{}}>
-        <div className="tw-grid tw-gap-x-4 tw-items-start dashboard-content container tw-mb-60">
+        <div className="tw-grid tw-gap-x-6 tw-items-start dashboard-content container tw-mb-60">
           <NavigationDesktop />
           <div className="">
             <UserRow />
