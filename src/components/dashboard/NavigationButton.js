5e91abe1865791b7f8ecb7e465fabbde7df268f6
Milad Khorramian <mahdiar.khorramian@yahoo.com>
2021-12-05 11:05:14 +0330

    z-index changed

--- src/components/dashboard/NavigationButton.js
+++ src/components/dashboard/NavigationButton.js
@@ -1,30 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { showDashboardMobileMenu } from '../../app/redux/actions/dashboardActions';
-import homeIcon from '../../assets/icons/Home.svg';
+import homeIcon from '../../assets/icons/Home White.svg';
 
 export default function NavigationButton() {
-  const [scroll, setScroll] = useState(window.scrollY);
-  const { show } = useSelector((state) => state.dashboard.navigation.mobile);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    window.addEventListener('scroll', () => {
-      setScroll(window.scrollY);
-    });
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <button
-      className="tw-fixed tw-bottom-4 tw-p-4 bg-primary scroll-to-top-button"
+      className="tw-fixed tw-bottom-4 tw-p-4 bg-primary scroll-to-top-button tw-block lg:tw-hidden"
       onClick={() => dispatch(showDashboardMobileMenu(true))}
       style={{
-        left: scroll !== (0, 0) ? '1rem' : '-10rem',
+        left: '1rem',
         transition: 'left .85s ease-out',
-        zIndex: '999999',
+        zIndex: '999',
       }}
     >
-      <img src={homeIcon} alt="" style={{ transform: 'rotate(90deg)' }} />
+      <img src={homeIcon} alt="بخش‌ها" />
     </button>
   );
 }
