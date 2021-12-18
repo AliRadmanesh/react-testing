338a79d16c1bcad094cbbe952cf1e9a318e37137
Milad Khorramian <mahdiar.khorramian@yahoo.com>
2021-12-18 10:24:07 +0330

    user authentication added for comments

--- src/components/course/UserComment.js
+++ src/components/course/UserComment.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Rating from 'react-rating';
 import toast from 'react-hot-toast';
 import { TextArea } from '../../common/template/TextArea';
@@ -12,12 +13,14 @@ import instance from '../../app/instance';
 
 export default function UserComment({ id }) {
   const [rate, setRate] = useState(0);
-  const [state, setState] = useState(null);
   const [message, setMessage] = useState('');
   const ref = useRef();
 
   const dispatch = useDispatch();
-  const { is_anonymous } = useSelector((store) => store.course.data);
+  const { is_anonymous } = useSelector((state) => state.course.data);
+  const {
+    user: { authenticated },
+  } = useSelector((state) => state.auth);
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -60,7 +63,7 @@ export default function UserComment({ id }) {
   };
 
   useEffect(() => {
-    document.querySelector('.stars').style.display = 'grid';
+    if (document.querySelector('.stars')) document.querySelector('.stars').style.display = 'grid';
   }, []);
 
   return (
@@ -72,48 +75,64 @@ export default function UserComment({ id }) {
         className="course-page-user-comment bg-white tw-p-4 tw-rounded-xl tw-shadow-md tw-w-full font-kalameh-num tw-mx-auto"
         style={{ maxWidth: '1000px' }}
       >
-        <p className="tw-text-xs tw-text-center tw-font-normal text-dark 2xl:tw-text-base">
-          شما می‌توانید دیدگاه خود را درباره این دوره با دیگر کاربران به اشتراک بگذارید.
-        </p>
+        {authenticated ? (
+          <>
+            <p className="tw-text-xs tw-text-center tw-font-normal text-dark 2xl:tw-text-base">
+              شما می‌توانید دیدگاه خود را درباره این دوره با دیگر کاربران به اشتراک بگذارید.
+            </p>
 
-        <div
-          className="tw-mx-auto tw-my-4 tw-w-1/3  lg:tw-w-3/4 "
-          style={{ minWidth: '152px', maxWidth: '224px', direction: 'ltr' }}
-        >
-          <Rating
-            className="tw-grid-cols-5 stars tw-justify-items-center"
-            onChange={(value) => {
-              setRate(value);
-            }}
-            initialRating={rate}
-            emptySymbol={<img src={starIcon} alt="" className="icon" />}
-            fullSymbol={<img src={starFillIcon} alt="" className="icon" />}
-          />
-        </div>
-
-        {rate !== 0 && (
-          <p className="tw-text-xs tw-font-normal 2xl:tw-text-base tw-text-center text-dark">
-            امتیاز شما برای این دوره {rate} از ۵ است؟
-          </p>
-        )}
-
-        <form className="tw-mt-4" onSubmit={onSubmit}>
-          <TextArea
-            ref={ref}
-            onChange={(e) => setMessage(e.target.value)}
-            value={message || null}
-            placeholder="دیدگاه خود را بنویسید..."
-            // message={uiMessage}
-          />
-          <div className="tw-flex md:tw-justify-end">
-            <button
-              type="submit"
-              className="button-primary tw-w-full md:tw-w-auto button-padding tw-text-center md:tw-ml-0"
+            <div
+              className="tw-mx-auto tw-my-4 tw-w-1/3  lg:tw-w-3/4 "
+              style={{ minWidth: '152px', maxWidth: '224px', direction: 'ltr' }}
             >
-              ارسال دیدگاه
-            </button>
+              <Rating
+                className="tw-grid-cols-5 stars tw-justify-items-center"
+                onChange={(value) => {
+                  setRate(value);
+                }}
+                initialRating={rate}
+                emptySymbol={<img src={starIcon} alt="" className="icon" />}
+                fullSymbol={<img src={starFillIcon} alt="" className="icon" />}
+              />
+            </div>
+
+            {rate !== 0 && (
+              <p className="tw-text-xs tw-font-normal 2xl:tw-text-base tw-text-center text-dark">
+                امتیاز شما برای این دوره {rate} از ۵ است؟
+              </p>
+            )}
+            <form className="tw-mt-4" onSubmit={onSubmit}>
+              <TextArea
+                ref={ref}
+                onChange={(e) => setMessage(e.target.value)}
+                value={message || null}
+                placeholder="دیدگاه خود را بنویسید..."
+                // message={uiMessage}
+              />
+              <div className="tw-flex md:tw-justify-end">
+                <button
+                  type="submit"
+                  className="button-primary tw-w-full md:tw-w-auto button-padding tw-text-center md:tw-ml-0"
+                >
+                  ارسال دیدگاه
+                </button>
+              </div>
+            </form>
+          </>
+        ) : (
+          <div className="tw-grid tw-justify-center">
+            <p className="tw-text-sm text-dark font-kalameh-num tw-font-normal 2xl:tw-text-lg 2xl:tw-mb-2 tw-text-center">
+              کاربر گرامی، خواهشمندیم،‌ برای ارسال دیدگاه، وارد سایت شده یا ثبت‌نام کنید.
+            </p>
+            <Link
+              to="/auth"
+              className="button-primary tw-mx-auto tw-text-center tw-mt-4 tw-text-sm tw-font-medium font-kalameh-num 2xl:tw-text-lg 2xl:tw-font-semibold tw-mr-4"
+              style={{ width: '200px', margin: '1rem auto 0' }}
+            >
+              ورود/ثبت‌نام
+            </Link>
           </div>
-        </form>
+        )}
       </div>
     </div>
   );
