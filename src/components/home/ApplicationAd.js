e143d98d9a5924a770abd21ee47be20521849589
AliRadmanesh <ali.radmanesh1995@gmail.com>
2021-12-21 16:47:57 +0330

    Add application links

--- src/components/home/ApplicationAd.js
+++ src/components/home/ApplicationAd.js
@@ -35,8 +35,16 @@ export default function ApplicationAd() {
                 کارساز همه آموزش ها توی جیبته
               </p>
               <div className="tw-grid tw-grid-cols-2 tw-gap-x-4 tw-mt-4 tw-mx-auto tw-max-w-md lg:tw-mr-0">
-                <img src={bazaar} alt="" />
-                <img src={playstore} alt="" />
+                <a href="https://cafebazaar.ir/app/com.karsaz.app" target="_blank" rel="noreferrer">
+                  <img src={bazaar} alt="" />
+                </a>
+                <a
+                  href="https://play.google.com/store/apps/details?id=com.karsaz.app"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={playstore} alt="" />
+                </a>
               </div>
             </div>
           </div>
@@ -48,50 +56,3 @@ export default function ApplicationAd() {
     </>
   );
 }
-
-// function prevAd() {
-//   return (
-//     <div className=" tw-w-full tw-h-screen tw-relative tw-py-12">
-//       <div className="md:tw-hidden tw-block tw-overflow-hidden">
-//         <img
-//           src={app}
-//           alt=""
-//           className="tw-h-auto tw-bg-transparent tw-max-w-max tw-relative"
-//           style={{ width: '130vw', left: '30%' }}
-//         />
-//       </div>
-//       <div className="tw-w-full tw-h-full application-ad-container">
-//         <div
-//           className="application-ad tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-justify-between tw-items-center tw-relative tw--top-28 md:tw-static md:tw-top-0"
-//           style={
-//             {
-//               // clipPath: 'polygon(0 90%, 0 100%, 100% 100%, 100% 0 )',
-//               // background: 'linear-gradient(to bottom, rgba(17, 138, 178, .08), rgba(17, 138, 178, .01))',
-//               // background: `url("${bg}") no-repeat center right/cover`,
-//             }
-//           }
-//         >
-//           <div className="tw-w-full md:tw-w-2/3 tw-mx-auto tw-px-4 md:tw-px-0">
-//             <h1
-//               className="text-black tw-font-extrabold 2xl:tw-font-black"
-//               style={{ fontSize: window.innerWidth <= 1536 ? '22px' : '34px' }}
-//             >
-//               اپلیکیشن کارساز
-//             </h1>
-//             <p className="tw-mt-4 text-black font-iranyekan tw-font-normal tw-text-xs 2xl:tw-text-lg">
-//               دسترسی آسان و راحت به دوره های آموزشی و فرصت های .شغلی، با استفاده از اپلیکیشن کارساز
-//               همه آموزش ها توی جیبته
-//             </p>
-//             <div className="tw-grid tw-grid-cols-2 tw-gap-x-4 tw-mt-4 tw-mx-auto tw-max-w-md lg:tw-mr-0">
-//               <img src={bazaar} alt="" />
-//               <img src={playstore} alt="" />
-//             </div>
-//           </div>
-//           <div className="tw-hidden md:tw-block tw-h-full md:tw-h-3/4 lg:tw-h-1/2 tw-ml-0">
-//             <img src={app} alt="" className="tw-w-full tw-h-auto" />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
